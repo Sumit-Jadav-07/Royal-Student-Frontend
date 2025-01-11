35a639cd561ef236f5f134e58dfcce843f561cae
Sumit Jadav <sumitroyaltechnosoft7@gmail.com>
2025-01-11 17:48:16 +0530

    Loader updated

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -112,14 +112,6 @@ function Student() {
         onAddStudentClick={toggleAddStudentModal}
       />
 
-      {/* Loading spinner displayed when `loading` is true */}
-      {loading && console.log("Loading spinner")}
-      {loading && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30">
-            <div className="w-10 h-10 border-4 border-blue-500 rounded-full animate-spin border-t-transparent"></div>
-        </div>
-      )}
-
       {/* Display student details */}
       <StudentDetails
         student={selectedStudent}
@@ -143,6 +135,14 @@ function Student() {
             setIsSubmitting={setIsSubmitting}
             setLoading={setLoading}
           />
+        </div>
+      )}
+
+      {/* Loading spinner displayed when `loading` is true */}
+      {loading && console.log("Loading spinner")}
+      {loading && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30">
+            <div className="w-10 h-10 border-4 border-blue-500 rounded-full animate-spin border-t-transparent"></div>
         </div>
       )}
     </div>
